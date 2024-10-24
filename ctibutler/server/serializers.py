9835d4b76115838e31b6c39edacf66a094ab18b9
Fadl <chaos@efqr.dev>
2024-10-24 15:33:57 +0100

    remove cve/cpe modes from CTI processor #3

--- ctibutler/server/serializers.py
+++ ctibutler/server/serializers.py
@@ -5,9 +5,6 @@ from rest_framework import serializers, validators
 ACP_MODES = {
     "capec-attack": "Relate CAPEC objects to ATT&CK objects",
     "cwe-capec": "Relate CWE objects to CAPEC objects",
-    "cve-cpe": "Relate CVE objects to CPE objects",
-    "cve-cwe": "Relate CVE objects to CWE objects",
-    "cve-epss": "Add EPSS Note(s) for CVE objects",
 }
 
 class StixObjectsSerializer(serializers.Serializer):
