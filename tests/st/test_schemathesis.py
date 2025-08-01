7ce74738d03a42538494bc8ced37ccc5fe798ab0
Fadl <chaos@efqr.dev>
2025-08-01 12:56:56 +0100

    use transport in tests

--- tests/st/test_schemathesis.py
+++ tests/st/test_schemathesis.py
@@ -1,14 +1,12 @@
-import time
 from unittest.mock import patch
-from urllib.parse import urlencode
 import schemathesis
 import pytest
-from schemathesis.core.transport import Response as SchemathesisResponse
 from ctibutler.wsgi import application as wsgi_app
-from rest_framework.response import Response as DRFResponse
 from hypothesis import settings
 from hypothesis import strategies
 from schemathesis.specs.openapi.checks import negative_data_rejection, positive_data_acceptance
+
+from tests.utils import Transport
 
 attack_ids = [
     "G0096",
@@ -27,17 +25,11 @@ capec_ids = [
 schema = schemathesis.openapi.from_wsgi("/api/schema/?format=json", wsgi_app)
 schema.config.base_url = "http://localhost:8006/"
 
-def make_wsgi_request(case: schemathesis.Case):
-    from django.test import Client
-    from schemathesis.transport.wsgi import WSGI_TRANSPORT
-    client = Client()
-    t = time.time()
-    serialized_request = WSGI_TRANSPORT.serialize_case(case)
-    serialized_request.update(QUERY_STRING=urlencode(serialized_request['query_string']))
-    response: DRFResponse = client.generic(**serialized_request)
-    elapsed = time.time() - t
-    return SchemathesisResponse(response.status_code, headers={k: [v] for k, v in response.headers.items()}, content=response.content, request=response.wsgi_request, elapsed=elapsed, verify=True)
-
+@pytest.fixture(autouse=True)
+def override_transport(monkeypatch, client):
+    ## patch transport.get
+    from schemathesis import transport
+    monkeypatch.setattr(transport, "get", lambda _: Transport())
 
 @pytest.mark.django_db(transaction=True)
 @schema.given(
@@ -51,13 +43,11 @@ def test_api(case: schemathesis.Case, **kwargs):
     for k, v in kwargs.items():
         if k in case.path_parameters:
             case.path_parameters[k] = v
-    call_resp = make_wsgi_request(case)
-    case.validate_response(call_resp, excluded_checks=[negative_data_rejection, positive_data_acceptance])
+    case.call_and_validate(excluded_checks=[negative_data_rejection, positive_data_acceptance])
 
 
 @pytest.mark.django_db(transaction=True)
 @schema.include(method="POST").parametrize()
 @patch('ctibutler.worker.tasks.create_celery_task_from_job')
 def test_imports(mock, case: schemathesis.Case):
-    call_resp = make_wsgi_request(case)
-    case.validate_response(call_resp, excluded_checks=[negative_data_rejection, positive_data_acceptance])
+    case.call_and_validate(excluded_checks=[negative_data_rejection, positive_data_acceptance])
